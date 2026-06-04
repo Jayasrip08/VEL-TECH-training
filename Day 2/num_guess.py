4b748de00dc1118565d61909e24a14b0315b2237
Jayasrip08 <jayasrip1808@gmail.com>
2026-06-04 09:52:18 +0530

    Update repository excluding Day 5 folder

--- Day 2/num_guess.py
+++ Day 2/num_guess.py
@@ -8,7 +8,9 @@ def check_guess(secret, guess):
         return "Too low! Try higher"
     else:
         return "Too high! Try lower"
+
 secret = random.randint(1, 10)
+
 for attempt in range(3):
     guess = int(input("Guess: "))
     print(check_guess(secret, guess))
